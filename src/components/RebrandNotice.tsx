7e84ecfc8760dd10c4de4afb4a7e77c66c9833dc
Enriquefft <enriquefft2001@gmail.com>
2025-07-02 12:08:22 -0500

    feat: updated site

--- src/components/RebrandNotice.tsx
+++ src/components/RebrandNotice.tsx
@@ -2,7 +2,6 @@ import { Instagram, Linkedin } from "lucide-react";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 import type { SVGProps } from "react";
-import { Countdown } from "@/components/Countdown";
 import { Button } from "@/components/ui/button";
 
 function TiktokIcon(props: SVGProps<SVGSVGElement>) {
@@ -20,24 +19,6 @@ export default function Page() {
 		<>
 			{/*biohack to bg fade*/}
 			<div className="min-h-screen items-center justify-center px-8 py-12 space-y-32 text-center ">
-				<div className="bg-gradient-to-br from-biohack to-secondary text-center p-8 opacity-50 grid grid-cols-1 md:grid-cols-2 gap-8 rounded-lg shadow-lg">
-					<Image
-						src="/Mascot_bio.png"
-						alt="Tardi the Mascot"
-						width={320}
-						height={320}
-						className="mx-auto drop-shadow-xl"
-						priority
-					/>
-					<div className="my-auto">
-						<a href="https://lu.ma/iavg1gc5">
-							<p className="text-2xl font-bold shadow-lg text-white rounded-lg p-4 bg-gradient-to-r from-biohack to-secondary hover:from-secondary hover:to-biohack transition-colors duration-300">
-								{t("eventBanner")}
-							</p>
-						</a>
-						<Countdown eventTimeCST="2025-06-28T10:00:00" />
-					</div>
-				</div>
 				<div className="space-y-4 max-w-3xl mx-auto items-center ">
 					<h1 className="text-3xl font-bold md:text-5xl">{t("title")}</h1>
 					<p className="text-lg">{t("message")}</p>
@@ -78,6 +59,14 @@ export default function Page() {
 							</Button>
 						</div>
 					</div>
+					<Image
+						src="/Mascot.png"
+						alt="Tardi the Mascot"
+						width={320}
+						height={320}
+						className="mx-auto drop-shadow-xl"
+						priority
+					/>
 				</div>
 			</div>
 		</>
